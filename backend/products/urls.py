4c808f5d0b1a57ec1c914663761ba13b4693c107
Osama Muhamed <63379624+U-sama@users.noreply.github.com>
2023-11-28 01:04:11 +0200

    Mixins&GenericAPI View

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.ProductListCreateAPIView.as_view()),
-    path("<int:pk>/", views.product_detail_view),
+    #path("", views.ProductListCreateAPIView.as_view()),
+    path("", views.productMixinView.as_view()),
+    #path("<int:pk>/", views.product_detail_view),
+    path("<int:pk>/", views.productMixinView.as_view()),
     path("<int:pk>/update/", views.productUpdateAPIView.as_view()),
     path("<int:pk>/delete/", views.productDestroyAPIView.as_view())
 
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -9,6 +9,35 @@ from django.shortcuts import get_object_or_404
 from .models import Product
 from .serializers import ProductSerializer
 
+
+#All in one
+class productMixinView(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    generics.GenericAPIView
+):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def get(self, request, *args, **kwargs):
+        pk = kwargs.get("pk")
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+    def perform_create(self, serializer):
+        print(serializer.validated_data)
+        title = serializer.validated_data.get("title")
+        content = serializer.validated_data.get("content") or None
+        
+        if content is None:
+            content = title
+        serializer.save(content="This is a single view doing cool stuff", )
 
 class productDestroyAPIView(generics.DestroyAPIView): 
     queryset = Product.objects.all()
